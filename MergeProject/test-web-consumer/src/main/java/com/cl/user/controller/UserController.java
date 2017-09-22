515cdf6aea2fa0863ae9fdafe039d4324505dc1a
bjyushicong <bjyushicong@corp.netease.com>
2017-09-22 16:08:41 +0800

    修改程序bug

--- MergeProject/test-web-consumer/src/main/java/com/cl/user/controller/UserController.java
+++ MergeProject/test-web-consumer/src/main/java/com/cl/user/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.cl.user.controller;
 
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.cl.user.servicei.UserService;
 
 @Controller
